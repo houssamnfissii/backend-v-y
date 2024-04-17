7da82d728a9c2f32bc4dc749e21c54c2ddf0e1ff
Youssra <youssra.benmimoun1@gmail.com>
2024-04-17 20:55:42 +0100

    done

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -9,37 +9,6 @@ use Illuminate\Http\Response;
 
 class ReservationController extends Controller
 {
-    // public function store(Request $request)
-    // {
-    //     $id = $request->id;
-    //     $location = $request->location;
-    //     $pickuptime = Carbon::parse($request->pickuptime);
-    //     $dropofftime = Carbon::parse($request->dropofftime);
-
-    //     $car_exists = Reservation::where('car_id', $id)
-    //         ->where(function ($query) use ($pickuptime, $dropofftime) {
-    //             $query->where(function ($q) use ($pickuptime) {
-    //                 $q->where('start_date', '<=', $pickuptime)
-    //                     ->where('end_date', '>=', $pickuptime);
-    //             })->orWhere(function ($q) use ($dropofftime) {
-    //                 $q->where('start_date', '<=', $dropofftime)
-    //                     ->where('end_date', '>=', $dropofftime);
-    //             });
-    //         })
-    //         ->exists();
-    //     if($car_exists){
-    //         return response()->json(['message' => 'La voiture est déjà réservée'], Response::HTTP_CONFLICT);
-    //     }
-    //     $reservation = Reservation::create([
-    //         'start_date' => $pickuptime,
-    //         'end_date' => $dropofftime,
-    //         'car_pick_up_location' => $location,
-    //         'car_drop_off_location' => $location,
-    //         'car_id' => $id,
-    //         'client_id' => 1, //auth()->id()
-    //     ]);
-    //     return response()->json(['message'=>'Réservation ajoutée','reservation'=>$reservation]);
-    // }
     public function store(Request $request)
     {
         $id = $request->id;
@@ -62,7 +31,6 @@ class ReservationController extends Controller
             return response()->json(['message' => 'La voiture est déjà réservée'], Response::HTTP_CONFLICT);
         }
 
-        // Utilisation de la méthode insert pour insérer les données directement dans la table
         $reservationId = Reservation::insertGetId([
             'start_date' => $pickuptime,
             'end_date' => $dropofftime,
@@ -70,11 +38,10 @@ class ReservationController extends Controller
             'car_drop_off_location' => $location,
             'car_id' => $id,
             'client_id' => 1, //auth()->id()
-            'created_at' => now(), // Ajoute la date et l'heure actuelles comme valeur par défaut pour le champ created_at
-            'updated_at' => now(), // Ajoute la date et l'heure actuelles comme valeur par défaut pour le champ updated_at
+            'created_at' => now(), 
+            'updated_at' => now(), 
         ]);
 
-        // Recherche de la réservation nouvellement insérée
         $reservation = Reservation::find($reservationId);
 
         return response()->json(['message'=>'Réservation ajoutée','reservation'=>$reservation]);
